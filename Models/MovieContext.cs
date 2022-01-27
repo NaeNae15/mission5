fb4c70a0a03dd352ba92dde10ecd6ba4e00c72af
janae <ddlion15@byu.edu>
2022-01-27 00:06:23 -0700

    recommit

---
+++ Models/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace JoelHilton.Models
+{
+    public class MovieContext : DbContext
+    {
+        //constructor
+        public MovieContext (DbContextOptions<MovieContext> options) : base(options)
+        {
+            //stuff 
+        }
+
+        public DbSet<MovieResponses> Responses { get; set; }
+    }
+}
---
+++ Models/MovieResponses.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace JoelHilton.Models
+{
+    public class MovieResponses { 
+
+        [Key]
+        [Required]
+        public int MovieId { get; set; }
+
+        public string Category { get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public string Director { get; set; }
+
+        public int Rating { get; set; }
+
+        public bool Edited { get; set; }
+
+        public string LentTo { get; set; }
+
+        public string Notes { get; set; }
+
+    }
+}
