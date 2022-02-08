83174b52f9719391f3b0d8588b33c27b5b6ae472
janae <ddlion15@byu.edu>
2022-02-08 00:10:23 -0700

    mission 4

---
+++ Migrations/20220208062229_Initial.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JoelHilton.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Responses",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Category = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    Director = table.Column<string>(nullable: false),
+                    Rating = table.Column<string>(nullable: false),
+                    Edited = table.Column<bool>(nullable: false),
+                    LentTo = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Responses", x => x.MovieId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, "Action/Adventure", "Joss Whedon", false, null, null, "PG-13", "Avengers, The", 2012 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, "Comedy", "Richard Donner", false, null, null, "PG", "Maverick", 1994 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, "Comedy", "Chris Columbus", false, null, null, "PG-13", "Mrs. Doubtfire", 1993 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 4, "Action/Adventure", "Richard Donner", false, null, "Most Quotable", "PG-13", "Ladyhawke", 1985 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Responses");
+        }
+    }
+}
